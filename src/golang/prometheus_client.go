e07464bb6ed6ef112b8f043c20456cdebe5b3f8e
Tim Hynes <mrtimhynes@googlemail.com>
2019-09-16 08:24:30 +0100

    Added job status gauges
    Signed-off-by: Tim Hynes <mrtimhynes@googlemail.com>

--- src/golang/prometheus_client.go
+++ src/golang/prometheus_client.go
@@ -13,7 +13,6 @@ package main
 
 import (
 	"log"
-	"fmt"
 	"net/http"
 	"time"
 	"github.com/rubrikinc/rubrik-sdk-for-go/rubrikcdm"
@@ -46,6 +45,18 @@ var (
 		Name: "rubrik_misc_storage_bytes",
 		Help: "Miscellaneous storage in Rubrik cluster.",
 	})
+	rubrik24HSucceededJobs = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "rubrik_24h_succeeded_jobs",
+		Help: "Last 24 hours succeeded jobs in Rubrik cluster.",
+	})
+	rubrik24HFailedJobs = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "rubrik_24h_failed_jobs",
+		Help: "Last 24 hours failed jobs in Rubrik cluster.",
+	})
+	rubrik24HCancelledJobs = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "rubrik_24h_cancelled_jobs",
+		Help: "Last 24 hours cancelled jobs in Rubrik cluster.",
+	})
 )
 
 func init() {
@@ -56,42 +67,112 @@ func init() {
 	prometheus.MustRegister(rubrikSnapshotStorage)
 	prometheus.MustRegister(rubrikLivemountStorage)
 	prometheus.MustRegister(rubrikMiscStorage)
+	prometheus.MustRegister(rubrik24HSucceededJobs)
+	prometheus.MustRegister(rubrik24HFailedJobs)
+	prometheus.MustRegister(rubrik24HCancelledJobs)
 }
 
 func main() {
-	//start := time.Now()
-
 	rubrik, err := rubrikcdm.ConnectEnv()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// get storage summary
 	go func() {
 		for {
-			fmt.Println("Tick...")
 			storageStats,err := rubrik.Get("internal","/stats/system_storage")
 			if err != nil {
 				log.Fatal(err)
 			}
+			// get total storage stat
 			if total, ok := storageStats.(map[string]interface{})["total"].(float64); ok {
 				rubrikTotalStorage.Set(total)
 			}
+			// get used storage stat
 			if used, ok := storageStats.(map[string]interface{})["used"].(float64); ok {
 				rubrikUsedStorage.Set(used)
 			}
+			// get available storage stat
 			if avail, ok := storageStats.(map[string]interface{})["available"].(float64); ok {
 				rubrikAvailableSpace.Set(avail)
 			}
+			// get snapshot storage stat
 			if snapshot, ok := storageStats.(map[string]interface{})["snapshot"].(float64); ok {
 				rubrikSnapshotStorage.Set(snapshot)
 			}
+			// get live mount storage stat
 			if livemount, ok := storageStats.(map[string]interface{})["liveMount"].(float64); ok {
 				rubrikLivemountStorage.Set(livemount)
 			}
+			// get misc storage stat
 			if misc, ok := storageStats.(map[string]interface{})["miscellaneous"].(float64); ok {
 				rubrikMiscStorage.Set(misc)
 			}
 			time.Sleep(time.Duration(60) * time.Second)
+		}
+	}()
+
+	/*
+	// get node stats
+	go func() {
+		for {
+			nodes,err := rubrik.Get("internal","/node")
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, v := range nodes.(map[string]interface{})["data"].([]interface{}) {
+				thisNode := (v.(interface{}).(map[string]interface{})["id"])
+				nodeDetail,err := rubrik.Get("internal","/node/"+thisNode.(string))
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(nodeDetail.(map[string]interface{})["status"])
+				nodeStats,err := rubrik.Get("internal","/node/"+thisNode.(string)+"/stats")
+				if err != nil {
+					log.Fatal(err)
+				}
+				// get cpu stat
+
+				// get network throughput stats
+
+				fmt.Println(nodeStats.(map[string]interface{})["ipAddress"])
+			}
+			time.Sleep(time.Duration(5) * time.Second)
+		}
+	}()
+	*/
+
+	// get job stats
+	go func() {
+		for {
+			reportData,err := rubrik.Get("internal","/report?report_template=ProtectionTasksDetails&report_type=Canned") // get our protection tasks details report
+			if err != nil {
+				log.Fatal(err)
+			}
+			reports := reportData.(map[string]interface{})["data"].([]interface{})
+			report_id := reports[0].(map[string]interface{})["id"]
+			chartData,err := rubrik.Get("internal","/report/"+report_id.(string)+"/chart?chart_id=chart0") // get our chart for the report
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, v := range chartData.([]interface{}) {
+				dataColumns := v.(map[string]interface{})["dataColumns"]
+				for _, w := range dataColumns.([]interface{}) {
+					label := w.(map[string]interface{})["label"]
+					dataPoints := w.(map[string]interface{})["dataPoints"].([]interface{})
+					value := dataPoints[0].(map[string]interface{})["value"].(float64)
+					switch label {
+					case "Succeeded":
+						rubrik24HSucceededJobs.Set(value)
+					case "Failed":
+						rubrik24HFailedJobs.Set(value)
+					case "Canceled":
+						rubrik24HCancelledJobs.Set(value)
+					}
+				}
+			}
+			time.Sleep(time.Duration(1) * time.Hour)
 		}
 	}()
 
