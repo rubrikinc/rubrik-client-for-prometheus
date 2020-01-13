5de0299b12c1c1ac858ab6961d35e8c0ede368c2
Tim Hynes <mrtimhynes@googlemail.com>
2020-01-13 20:17:56 +0000

    refactored module

---
+++ src/golang/rubrik_live_mount.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"time"
+	"github.com/rubrikinc/rubrik-sdk-for-go/rubrikcdm"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	// live mount stats
+	rubrikMssqlLiveMountAge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "rubrik_mssql_live_mount_age_seconds",
+			Help: "Age of SQL DB live mounts.",
+		},
+		[]string{
+			"clusterName",
+			"sourceDatabaseName",
+			"sourceDatabaseId",
+			"mountedDatabaseName",
+		},
+	)
+)
+
+func init() {
+	// live mount stats
+	prometheus.MustRegister(rubrikMssqlLiveMountAge)
+}
+
+// GetMssqlLiveMounts ...
+func GetMssqlLiveMountAges(rubrik *rubrikcdm.Credentials, clusterName string) {
+	mountData,err := rubrik.Get("v1","/mssql/db/mount") // get our mssql live mount summary
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, v := range mountData.(map[string]interface{})["data"].([]interface{}) {
+		thisSourceDatabaseName := v.(map[string]interface{})["sourceDatabaseName"]
+		thisSourceDatabaseID := v.(map[string]interface{})["sourceDatabaseId"]
+		thisMountedDatabaseName := v.(map[string]interface{})["mountedDatabaseName"]
+		thisCreationDate := v.(map[string]interface{})["creationDate"]
+		mountTime, _ := time.Parse(time.RFC3339, thisCreationDate.(string))
+		age := time.Since(mountTime)
+		//fmt.Println(age.Seconds())
+		rubrikMssqlLiveMountAge.WithLabelValues(
+			clusterName,
+			thisSourceDatabaseName.(string),
+			thisSourceDatabaseID.(string),
+			thisMountedDatabaseName.(string)).Set(age.Seconds())
+	}
+}
---
+++ src/golang/rubrik_storage_stats.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"log"
+	"github.com/rubrikinc/rubrik-sdk-for-go/rubrikcdm"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	// storage stats
+	rubrikTotalStorage = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "rubrik_total_storage_bytes",
+			Help: "Total storage in Rubrik cluster.",
+		},
+		[]string{
+			"clusterName",
+		},
+	)
+	rubrikUsedStorage = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "rubrik_used_storage_bytes",
+			Help: "Used storage in Rubrik cluster.",
+		},
+		[]string{
+			"clusterName",
+		},
+	)
+	rubrikAvailableSpace = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "rubrik_available_storage_bytes",
+			Help: "Available storage in Rubrik cluster.",
+		},
+		[]string{
+			"clusterName",
+		},
+	)
+	rubrikSnapshotStorage = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "rubrik_snapshot_storage_bytes",
+			Help: "Snapshot storage in Rubrik cluster.",
+		},
+		[]string{
+			"clusterName",
+		},
+	)
+	rubrikLivemountStorage = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "rubrik_livemount_storage_bytes",
+			Help: "Live Mount storage in Rubrik cluster.",
+		},
+		[]string{
+			"clusterName",
+		},
+	)
+	rubrikMiscStorage = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "rubrik_misc_storage_bytes",
+			Help: "Miscellaneous storage in Rubrik cluster.",
+		},
+		[]string{
+			"clusterName",
+		},
+	)
+	rubrikRunwayRemaining = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "rubrik_runway_remaining",
+			Help: "Runway remaining, in days, on Rubrik cluster.",
+		},
+		[]string{
+			"clusterName",
+		},
+	)
+)
+
+func init() {
+	// storage stats
+	prometheus.MustRegister(rubrikTotalStorage)
+	prometheus.MustRegister(rubrikUsedStorage)
+	prometheus.MustRegister(rubrikAvailableSpace)
+	prometheus.MustRegister(rubrikSnapshotStorage)
+	prometheus.MustRegister(rubrikLivemountStorage)
+	prometheus.MustRegister(rubrikMiscStorage)
+	prometheus.MustRegister(rubrikRunwayRemaining)
+}
+
+// GetStorageSummaryStats ...
+func GetStorageSummaryStats(rubrik *rubrikcdm.Credentials, clusterName string) {
+	storageStats,err := rubrik.Get("internal","/stats/system_storage")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// get total storage stat
+	if total, ok := storageStats.(map[string]interface{})["total"].(float64); ok {
+		rubrikTotalStorage.WithLabelValues(clusterName).Set(total)
+	}
+	// get used storage stat
+	if used, ok := storageStats.(map[string]interface{})["used"].(float64); ok {
+		rubrikUsedStorage.WithLabelValues(clusterName).Set(used)
+	}
+	// get available storage stat
+	if avail, ok := storageStats.(map[string]interface{})["available"].(float64); ok {
+		rubrikAvailableSpace.WithLabelValues(clusterName).Set(avail)
+	}
+	// get snapshot storage stat
+	if snapshot, ok := storageStats.(map[string]interface{})["snapshot"].(float64); ok {
+		rubrikSnapshotStorage.WithLabelValues(clusterName).Set(snapshot)
+	}
+	// get live mount storage stat
+	if livemount, ok := storageStats.(map[string]interface{})["liveMount"].(float64); ok {
+		rubrikLivemountStorage.WithLabelValues(clusterName).Set(livemount)
+	}
+	// get misc storage stat
+	if misc, ok := storageStats.(map[string]interface{})["miscellaneous"].(float64); ok {
+		rubrikMiscStorage.WithLabelValues(clusterName).Set(misc)
+	}
+}
+
+// GetRunwayRemaining ...q
+func GetRunwayRemaining(rubrik *rubrikcdm.Credentials, clusterName string) {
+	runwayRemaining,err := rubrik.Get("internal","/stats/runway_remaining")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// get runway remaining stat
+	if runway, ok := runwayRemaining.(map[string]interface{})["days"].(float64); ok {
+		rubrikRunwayRemaining.WithLabelValues(clusterName).Set(runway)
+	}
+}
