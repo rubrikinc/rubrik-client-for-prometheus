9cb994d5c6474ecb7063a30df886add00ece71c4
Tim Hynes <mrtimhynes@googlemail.com>
2020-09-21 09:30:28 +0100

    added check for empty events set to failed job checker

--- src/golang/jobs/rubrik_failed_jobs.go
+++ src/golang/jobs/rubrik_failed_jobs.go
@@ -53,105 +53,49 @@ func GetMssqlFailedJobs(rubrik *rubrikcdm.Credentials, clusterName string) {
 		if err != nil {
 			log.Println("Error from jobs.GetMssqlFailedJobs: ",err)
 		}
-		for _, v := range eventData.(map[string]interface{})["data"].([]interface{}) {
-			thisEventSeriesID := v.(map[string]interface{})["eventSeriesId"]
-			eventSeriesData,err := rubrik.Get("internal","/event_series/"+thisEventSeriesID.(string))
-			if err != nil {
-				log.Println("Error from jobs.GetMssqlFailedJobs: ",err)
-			}
-			hasFailedEvent := false
-			for _, w := range eventSeriesData.(map[string]interface{})["eventDetailList"].([]interface{}) {
-				thisEventStatus := w.(map[string]interface{})["status"]
-				if thisEventStatus == "Failure" {
-					hasFailedEvent = true
+		if (eventData.(map[string]interface{})["data"] != nil) {
+			for _, v := range eventData.(map[string]interface{})["data"].([]interface{}) {
+				thisEventSeriesID := v.(map[string]interface{})["eventSeriesId"]
+				eventSeriesData,err := rubrik.Get("internal","/event_series/"+thisEventSeriesID.(string))
+				if err != nil {
+					log.Println("Error from jobs.GetMssqlFailedJobs: ",err)
 				}
-			}
-			if hasFailedEvent == true {
-				thisObjectName := v.(map[string]interface{})["objectInfo"].(map[string]interface{})["objectName"]
-				thisObjectID := v.(map[string]interface{})["objectInfo"].(map[string]interface{})["objectId"]
-				thisLocation := v.(map[string]interface{})["location"]
-				var thisStartTime string
-				if v.(map[string]interface{})["startTime"] == nil {
-					thisStartTime = "null"
-				} else {
-					thisStartTime = v.(map[string]interface{})["startTime"].(string)
-				}
-				var thisEndTime string
-				if v.(map[string]interface{})["endTime"] == nil {
-					thisEndTime = "null"
-				} else {
-					thisEndTime = v.(map[string]interface{})["endTime"].(string)
-				}
-				var thisLogicalSize string
-				if v.(map[string]interface{})["objectLogicalSize"] == nil {
-					thisLogicalSize = "null"
-				} else {
-					thisLogicalSize = strconv.FormatFloat(v.(map[string]interface{})["objectLogicalSize"].(float64), 'f', -1, 64)
-				}
-				var thisDuration string
-				if v.(map[string]interface{})["duration"] == nil {
-					thisDuration = "null"
-				} else {
-					thisDuration = v.(map[string]interface{})["duration"].(string)
-				}
-				thisEventDate := v.(map[string]interface{})["eventDate"]
-				rubrikMssqlFailedJob.WithLabelValues(
-					clusterName,
-					thisObjectName.(string),
-					thisObjectID.(string),
-					thisLocation.(string),
-					thisStartTime,
-					thisEndTime,
-					thisLogicalSize,
-					thisDuration,
-					thisEventDate.(string)).Set(1)
-			}
-		}
-	} else { // cluster version is 5.2 or newer
-		eventData,err := rubrik.Get("v1","/event/latest?event_status=Failure&event_type=Backup&object_type=Mssql")
-		if err != nil {
-			log.Println("Error from jobs.GetMssqlFailedJobs: ",err)
-		}
-		for _, v := range eventData.(map[string]interface{})["data"].([]interface{}) {
-			thisEventSeriesID := v.(map[string]interface{})["latestEvent"].(map[string]interface{})["eventSeriesId"]
-			eventSeriesData,err := rubrik.Get("v1","/event_series/"+thisEventSeriesID.(string))
-			if err != nil {
-				log.Println("Error from jobs.GetMssqlFailedJobs: ",err)
-			}
-			hasFailedEvent := false
-			for _, w := range eventSeriesData.(map[string]interface{})["eventDetailList"].([]interface{}) {
-				thisEventStatus := w.(map[string]interface{})["status"]
-				if thisEventStatus == "Failure" {
-					hasFailedEvent = true
+				hasFailedEvent := false
+				for _, w := range eventSeriesData.(map[string]interface{})["eventDetailList"].([]interface{}) {
+					thisEventStatus := w.(map[string]interface{})["status"]
+					if thisEventStatus == "Failure" {
+						hasFailedEvent = true
+					}
 				}
 				if hasFailedEvent == true {
-					thisObjectName := w.(map[string]interface{})["objectName"]
-					thisObjectID := w.(map[string]interface{})["objectId"]
-					thisLocation := w.(map[string]interface{})["location"]
+					thisObjectName := v.(map[string]interface{})["objectInfo"].(map[string]interface{})["objectName"]
+					thisObjectID := v.(map[string]interface{})["objectInfo"].(map[string]interface{})["objectId"]
+					thisLocation := v.(map[string]interface{})["location"]
 					var thisStartTime string
-					if w.(map[string]interface{})["startTime"] == nil {
+					if v.(map[string]interface{})["startTime"] == nil {
 						thisStartTime = "null"
 					} else {
-						thisStartTime = w.(map[string]interface{})["startTime"].(string)
+						thisStartTime = v.(map[string]interface{})["startTime"].(string)
 					}
 					var thisEndTime string
-					if w.(map[string]interface{})["endTime"] == nil {
+					if v.(map[string]interface{})["endTime"] == nil {
 						thisEndTime = "null"
 					} else {
-						thisEndTime = w.(map[string]interface{})["endTime"].(string)
+						thisEndTime = v.(map[string]interface{})["endTime"].(string)
 					}
 					var thisLogicalSize string
-					if w.(map[string]interface{})["logicalSize"] == nil {
+					if v.(map[string]interface{})["objectLogicalSize"] == nil {
 						thisLogicalSize = "null"
 					} else {
-						thisLogicalSize = strconv.FormatFloat(w.(map[string]interface{})["logicalSize"].(float64), 'f', -1, 64)
+						thisLogicalSize = strconv.FormatFloat(v.(map[string]interface{})["objectLogicalSize"].(float64), 'f', -1, 64)
 					}
 					var thisDuration string
-					if w.(map[string]interface{})["duration"] == nil {
+					if v.(map[string]interface{})["duration"] == nil {
 						thisDuration = "null"
 					} else {
-						thisDuration = w.(map[string]interface{})["duration"].(string)
+						thisDuration = v.(map[string]interface{})["duration"].(string)
 					}
+					thisEventDate := v.(map[string]interface{})["eventDate"]
 					rubrikMssqlFailedJob.WithLabelValues(
 						clusterName,
 						thisObjectName.(string),
@@ -160,7 +104,67 @@ func GetMssqlFailedJobs(rubrik *rubrikcdm.Credentials, clusterName string) {
 						thisStartTime,
 						thisEndTime,
 						thisLogicalSize,
-						thisDuration).Set(1)
+						thisDuration,
+						thisEventDate.(string)).Set(1)
+				}
+			}
+		}
+	} else { // cluster version is 5.2 or newer
+		eventData,err := rubrik.Get("v1","/event/latest?event_status=Failure&event_type=Backup&object_type=Mssql")
+		if err != nil {
+			log.Println("Error from jobs.GetMssqlFailedJobs: ",err)
+		}
+		if (eventData.(map[string]interface{})["data"] != nil) {
+			for _, v := range eventData.(map[string]interface{})["data"].([]interface{}) {
+				thisEventSeriesID := v.(map[string]interface{})["latestEvent"].(map[string]interface{})["eventSeriesId"]
+				eventSeriesData,err := rubrik.Get("v1","/event_series/"+thisEventSeriesID.(string))
+				if err != nil {
+					log.Println("Error from jobs.GetMssqlFailedJobs: ",err)
+				}
+				hasFailedEvent := false
+				for _, w := range eventSeriesData.(map[string]interface{})["eventDetailList"].([]interface{}) {
+					thisEventStatus := w.(map[string]interface{})["status"]
+					if thisEventStatus == "Failure" {
+						hasFailedEvent = true
+					}
+					if hasFailedEvent == true {
+						thisObjectName := w.(map[string]interface{})["objectName"]
+						thisObjectID := w.(map[string]interface{})["objectId"]
+						thisLocation := w.(map[string]interface{})["location"]
+						var thisStartTime string
+						if w.(map[string]interface{})["startTime"] == nil {
+							thisStartTime = "null"
+						} else {
+							thisStartTime = w.(map[string]interface{})["startTime"].(string)
+						}
+						var thisEndTime string
+						if w.(map[string]interface{})["endTime"] == nil {
+							thisEndTime = "null"
+						} else {
+							thisEndTime = w.(map[string]interface{})["endTime"].(string)
+						}
+						var thisLogicalSize string
+						if w.(map[string]interface{})["logicalSize"] == nil {
+							thisLogicalSize = "null"
+						} else {
+							thisLogicalSize = strconv.FormatFloat(w.(map[string]interface{})["logicalSize"].(float64), 'f', -1, 64)
+						}
+						var thisDuration string
+						if w.(map[string]interface{})["duration"] == nil {
+							thisDuration = "null"
+						} else {
+							thisDuration = w.(map[string]interface{})["duration"].(string)
+						}
+						rubrikMssqlFailedJob.WithLabelValues(
+							clusterName,
+							thisObjectName.(string),
+							thisObjectID.(string),
+							thisLocation.(string),
+							thisStartTime,
+							thisEndTime,
+							thisLogicalSize,
+							thisDuration).Set(1)
+					}
 				}
 			}
 		}
